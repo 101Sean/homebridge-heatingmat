b0b217e59de9a67af59ec0e64bc5d02433f85f84
mikeryu98 <mikeryu98@gmail.com>
2026-01-15 00:53:41 +0900

    v1.7 1. 연결 안정성 개선

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const HeatingMatAccessory = require('./lib/HeatingMatAccessory');
 
+class HeatingMatPlatform {
+    constructor(log, config, api) {
+        this.log = log;
+        this.config = config;
+        this.api = api;
+
+        this.api.on('didFinishLaunching', () => {
+            this.config.devices.forEach(deviceConfig => {
+                this.log.info(`액세서리 로드 중: ${deviceConfig.name}`);
+                new HeatingMatAccessory(this.log, deviceConfig, this.api);
+            });
+        });
+    }
+}
+
 module.exports = (api) => {
-    api.registerAccessory('homebridge-heatingmat', 'Heating Mat', HeatingMatAccessory);
+    api.registerPlatform('homebridge-heatingmat', 'Heating Mat Platform', HeatingMatPlatform);
 };
