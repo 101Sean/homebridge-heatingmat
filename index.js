657affaecbe3f15a7897b6895fbf44179885d193
mikeryu98 <mikeryu98@gmail.com>
2026-01-15 01:07:37 +0900

    v1.7 1. 연결 안정성 개선

--- index.js
+++ index.js
@@ -3,13 +3,31 @@ const HeatingMatAccessory = require('./lib/HeatingMatAccessory');
 class HeatingMatPlatform {
     constructor(log, config, api) {
         this.log = log;
-        this.config = config;
+        this.config = config || {};
         this.api = api;
+
+        if (!this.config.devices || !Array.isArray(this.config.devices)) {
+            return;
+        }
 
         this.api.on('didFinishLaunching', () => {
             this.config.devices.forEach(deviceConfig => {
-                this.log.info(`액세서리 로드 중: ${deviceConfig.name}`);
-                new HeatingMatAccessory(this.log, deviceConfig, this.api);
+                try {
+                    this.log.info(`기기 등록 시도: ${deviceConfig.name}`);
+
+                    const accessoryInstance = new HeatingMatAccessory(this.log, deviceConfig, this.api);
+
+                    this.api.publishExternalAccessories('homebridge-heatingmat', [
+                        {
+                            category: this.api.hap.Categories.THERMOSTAT,
+                            external: true,
+                            services: accessoryInstance.getServices(),
+                            displayName: deviceConfig.name || 'Heating Mat'
+                        }
+                    ]);
+                } catch (err) {
+                    this.log.error(`기기 등록 중 오류 발생: ${err.message}`);
+                }
             });
         });
     }
